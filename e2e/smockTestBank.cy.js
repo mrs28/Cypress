0aa454eb4b3a280c2a1b429fa284b886e48dec36
mrs28 <la.reina.bosque@gmail.com>
2023-08-10 20:30:37 -0500

    se agrega prueba de actualización del gráfico

--- e2e/smockTestBank.cy.js
+++ e2e/smockTestBank.cy.js
@@ -12,7 +12,15 @@ describe("Test suite - conjunto de pruebas", () => {
         
     })
 
-    it ("Validar página 3", ()=>{
-        
+    it ("Prueba de actualización del gráfico", ()=>{
+        cy.visit("http://zero.webappsecurity.com/")
+        cy.get("#signin_button").click()
+        cy.get("#user_login").type("username")
+        cy.get("#user_password").type("password")
+        cy.get(".btn").click()
+        cy.get("#money_map_tab > a").click()
+        cy.get("#ext-sprite-1259").should("be.visible")
+        cy.get("#ext-sprite-1167 >tspan").click()
+        cy.get("#ext-sprite-1259").should("be.visible")
     })
 })
